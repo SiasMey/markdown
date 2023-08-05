4473687f0c029a95da34f039e3157835a51bad9c
Sias Mey <siamey@gmail.com>
2023-08-05 11:36:23 +0200

    test: refactor test result messages

--- parse/symbols/symbols_test.go
+++ parse/symbols/symbols_test.go
@@ -5,6 +5,40 @@ import (
 	"testing"
 )
 
+func TestSymbolsShouldReturnTitle(t *testing.T) {
+	input := "# Title"
+	expected := "Title"
+
+	res, err := Extract(input)
+	if res.Title.Value != expected {
+		failMessageString(t, input, res.Title.Value, err, expected)
+	}
+}
+
+func TestSymbolsShouldReturnTitleCharStart(t *testing.T) {
+	input := "# Title"
+	expected := 0
+
+	res, err := Extract(input)
+	if res.Title.CharStart != expected {
+		failMessageInt(t, input, res.Title.CharStart, err, expected)
+	}
+}
+
+func TestSymbolsShouldReturnWikilinks(t *testing.T) {
+	input := "[[wikilink]]"
+	expected := "wikilink"
+
+	res, err := Extract(input)
+	if len(res.Wikilinks) != 1 {
+		failMessageInt(t, input, len(res.Wikilinks), err, 1)
+	}
+	link := res.Wikilinks[0]
+	if link.Value != expected {
+		failMessageString(t, input, link.Value, err, expected)
+	}
+}
+
 func itemExists(slice interface{}, item interface{}) bool {
 	s := reflect.ValueOf(slice)
 
@@ -21,45 +55,10 @@ func itemExists(slice interface{}, item interface{}) bool {
 	return false
 }
 
-func TestSymbolsShouldReturnTitle(t *testing.T) {
-	input := "# Title"
-	expected := "Title"
-
-	res, err := Extract(input)
-	if res.Title.Value != expected {
-		t.Fatalf(`Extract(%s) = %q, %v, expected %s`, input, res.Title.Value, err, expected)
-	}
-}
-
-func TestSymbolsShouldReturnTitleCharStart(t *testing.T) {
-	input := "# Title"
-	expected := 0
-
-	res, err := Extract(input)
-	if res.Title.CharStart != expected {
-		t.Fatalf(`Extract(%s) = %d, %v, expected %d`, input, res.Title.CharStart, err, expected)
-	}
-}
-
 func failMessageString(t *testing.T, input string, result string, err error, expected string) {
 	t.Fatalf(`Extract(%s) = %q, %v, expected %s`, input, result, err, expected)
 }
 
 func failMessageInt(t *testing.T, input string, result int, err error, expected int) {
 	t.Fatalf(`Extract(%s) = %d, %v, expected %d`, input, result, err, expected)
-}
-
-func TestSymbolsShouldReturnWikilinks(t *testing.T) {
-	input := "[[wikilink]]"
-	expected := "wikilink"
-
-	res, err := Extract(input)
-	if len(res.Wikilinks) < 1 {
-		t.Fatalf(`Extract(%s) Expected at least one wikilink: %q, %v`, input, res.Wikilinks, err)
-	}
-
-	link := res.Wikilinks[0]
-	if link.Value != expected {
-		t.Fatalf(`Extract(%s) = %q, %v, expected %s`, input, res.Wikilinks, err, expected)
-	}
 }
